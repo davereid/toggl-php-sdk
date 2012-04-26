4de26f5bbeb22d39104f2630587b77b7b416a6af
Dave Reid <dave@davereid.net>
2012-04-26 01:57:28 -0500

    Store data in TogglRecord->data rather than setting direct object properties.

--- src/TogglRecord.php
+++ src/TogglRecord.php
@@ -4,15 +4,46 @@ abstract class TogglRecord {
   static $element_name;
   static $element_plural_name;
 
-  function __construct(TogglConnection $connection, array $values = array()) {
-    $this->connection = $connection;
-    $this->setData($values);
+  private $data = array();
+  private $connection;
+
+  public function __get($name) {
+    if (array_key_exists($name, $this->data)) {
+      return $this->data[$name];
+    }
   }
 
-  function setData(array $values) {
-    foreach ($values as $key => $value) {
-      $this->{$key} = $value;
-    }
+  public function __set($name, $value) {
+    $this->data[$name] = $value;
+  }
+
+  public function __isset($name) {
+    return isset($this->data[$name]);
+  }
+
+  public function __unset($name) {
+    unset($this->data[$name]);
+  }
+
+  protected function setConnection(TogglConnection $connection) {
+    $this->connection = $connection;
+  }
+
+  public function getConnection() {
+    return $this->connection;
+  }
+
+  protected function setData(array $data) {
+    $this->data = $data;
+  }
+
+  public function getData() {
+    return $this->data;
+  }
+
+  function __construct(TogglConnection $connection, array $data = array()) {
+    $this->connection = $connection;
+    $this->data = $data;
   }
 
   public static function load(TogglConnection $connection, $id) {
@@ -40,10 +71,11 @@ abstract class TogglRecord {
 
   public function save() {
     $options['method'] = !empty($this->id) ? 'PUT' : 'POST';
-    $options['data'][$this::$element_name] = $this;
+    $options['data'][$this::$element_name] = $this->data;
     $url = $this::element_plural_name . (!empty($this->id) ? '/' . $this->id : '');
     $response = $this->connection->request($this->getURL($url), $options);
-    $this->setData($response->data['data']);
+    $this->data = $response->data['data'];
+    return TRUE;
   }
 
   public function delete() {
@@ -52,5 +84,7 @@ abstract class TogglRecord {
       $url = $this::$element_plural_name . '/' . $this->id;
       $response = $this->connection->request($this->getURL($url), $options);
     }
+    $this->data = array();
+    return TRUE;
   }
 }
